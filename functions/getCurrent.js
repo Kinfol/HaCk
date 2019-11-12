6097cfc52e2cec411959260259453f3d3408bb77
Chris Williams <Chris.Williams@dvla.gov.uk>
2019-11-12 12:53:11 +0000

    working get

--- functions/getCurrent.js
+++ functions/getCurrent.js
@@ -3,20 +3,16 @@
 const AWS = require('aws-sdk');
 // Set the region 
 AWS.config.update({region: 'eu-west-1'});
-const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 
 module.exports.handler = async (event, context) => {
   console.log(event);
 
   var params = {
-    Key: {
-     "name": {
-       S: event.pathParameters.name,
-      }
-    }, 
+    Key: { name:  event.pathParameters.name}, 
     TableName: process.env.parking_area_current_table
   };
-  var results = await ddb.getItem(params).promise();
+  var results = await ddb.get(params).promise();
 
   return {
     statusCode: 200,
