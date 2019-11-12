6b2bdad785432c70b66b042464dfe65faa693686
Chris Williams <Chris.Williams@dvla.gov.uk>
2019-11-12 11:13:14 +0000

    get for dynamodb

--- functions/getCurrent.js
+++ functions/getCurrent.js
@@ -1,6 +1,21 @@
 'use strict';
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'eu-west-1'});
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 module.exports.handler = async event => {
+
+  var params = {
+    Key: {
+     "name": {
+       S: "morriston"
+      }
+    }, 
+    TableName: process.env.parking_area_current_table
+  };
+  var results = await ddb.getItem(params).promise();
   const parking_area = [{
     "WEST": 5
   },
@@ -16,7 +31,8 @@ module.exports.handler = async event => {
     body: JSON.stringify(
       {
         parking_area: parking_area,
-        total_space: '10'
+        total_space: '10',
+        results: results,
       },
       null,
       2
